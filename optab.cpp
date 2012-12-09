f555121f3b84db21630ae367557c6b7fc071b862
ltroot <xi2an.chen@gmail.com>
2012-12-09 12:02:08 +0800

    第三次作業

--- optab.cpp
+++ optab.cpp
@@ -2,7 +2,10 @@
  * @author 陳賢安(9903089A)
  * @encode UTF-8
  *
+ * 全部是自己寫的
+ *
  * 功能：
+ * #指令碼&位址對應
  * #從檔案讀取opcode table
  * #search
  * #insert
@@ -15,9 +18,12 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 using std::istringstream;
+using std::stringstream;
 using std::ifstream;
+using std::ofstream;
 using std::string;
 using std::map;
 using std::cout;
@@ -25,37 +31,56 @@ using std::endl;
 using std::hex;
 using std::showbase;
 using std::uppercase;
+using std::setw;
 
 enum syntaxType{
-	AssemblerDirectives,
-	Instruction
+	AssemblerDirectives = 1,
+	Instruction,
+	NOT
 };
 
 class HashTable{
-	public:
-		struct record{
-			enum syntaxType type;
-			string mnemonic;
-			unsigned char opcode;
-			unsigned int format;
-		};
-
-		HashTable();
-
-		void search(string key);
-		void insert(syntaxType type,
-				    string mnemonic,
-					unsigned char opcode,
-					unsigned int format);
-		void Delete(string key);
-		void traversal();
-		void readOptabFromFile(const char * fileName);
-	private:
-		map<string, struct record> table_;
+public:
+    struct record{
+        enum syntaxType type;
+        string mnemonic;
+        unsigned char opcode;
+        unsigned int format;
+    };
+    
+    class keyToValue{
+    public:
+        keyToValue(const string& first):first_(first){}
+        bool operator() (const map<string, struct record>::value_type& value)
+        {
+            return value.first == first_ ? true : false;
+        }
+    private:
+        const string& first_;
+    };
+    
+    HashTable();
+    
+    void insert(syntaxType type,
+                string mnemonic,
+                unsigned char opcode,
+                unsigned int format);
+    void Delete(string key);
+    void readOptabFromFile(const char * fileName);
+    const record& search(string first);
+    string parseInstruction(string, unsigned int&);
+private:
+    map<string, struct record> table_;
+    record nullRecord;
 };
 
 HashTable::HashTable()
 {
+	nullRecord.type = NOT;
+	nullRecord.format = 0;
+	nullRecord.mnemonic = "";
+	nullRecord.opcode = 0;
+    
 	this->insert(Instruction, "ADD", 0x18, 3);
 	this->insert(Instruction, "ADDF", 0x58, 3);
 	this->insert(Instruction, "ADDR", 0x90, 2);
@@ -115,33 +140,12 @@ HashTable::HashTable()
 	this->insert(Instruction, "TIX", 0x2C, 3);
 	this->insert(Instruction, "TIXR", 0xB8, 2);
 	this->insert(Instruction, "WD", 0xDC, 3);
-	/*this->insert(AssemblerDirectives, "START", 0x01, NULL);
+	this->insert(AssemblerDirectives, "START", 0x01, NULL);
 	this->insert(AssemblerDirectives, "END", 0x02, NULL);
 	this->insert(AssemblerDirectives, "BYTE", 0x03, NULL);
 	this->insert(AssemblerDirectives, "WORD", 0x04, NULL);
 	this->insert(AssemblerDirectives, "RESB", 0x05, NULL);
-	this->insert(AssemblerDirectives, "RESW", 0x06, NULL);*/
-}
-
-void HashTable::search(string key)
-{
-	record temp;
-	map<string, struct record>::iterator it = table_.find(key);
-	
-	if(it != table_.end())
-	{
-		temp = it->second;
-		if(temp.type == Instruction)
-		{
-			cout << key << ", opcode=";
-			cout.width(2);
-			cout.fill('0');
-			cout << uppercase << hex << (temp.opcode + 0)
-				 << ", format=" << temp.format << endl;
-		}
-		else cout << key << ", Assembler Directive" << endl;
-	}
-	else cout << key << ", not found" <<endl;
+	this->insert(AssemblerDirectives, "RESW", 0x06, NULL);
 }
 
 void HashTable::insert(syntaxType type, string mnemonic, unsigned char opcode, unsigned int format)
@@ -152,22 +156,6 @@ void HashTable::insert(syntaxType type, string mnemonic, unsigned char opcode, u
 	temp.opcode = opcode;
 	temp.format = format;
 	table_.insert(map<string, struct record>::value_type(mnemonic, temp));
-}
-
-void HashTable::traversal()
-{
-	map<string, struct record>::const_iterator index = table_.begin();
-	record temp;
-	for(index; index != table_.end(); ++index)
-	{
-		temp = index->second;
-		cout << temp.mnemonic << " -> ";
-		cout << "0x";
-		cout.width(2);
-		cout.fill('0');
-		cout << uppercase << hex << (temp.opcode + 0) << " -> ";
-		cout << temp.format << endl;
-	}
 }
 
 void HashTable::Delete(string key)
@@ -184,7 +172,7 @@ void HashTable::readOptabFromFile(const char * fileName)
 	unsigned char opcode;
 	unsigned int c;
 	unsigned int format;
-
+    
 	while( std::getline(inputFile, buffer) )
 	{
 		istringstream iss(buffer);
@@ -194,33 +182,174 @@ void HashTable::readOptabFromFile(const char * fileName)
 		opcode = c;
 		this->insert(Instruction, mnemonic, opcode, format);
 	}
-
+    
 	inputFile.close();
+}
+
+const HashTable::record& HashTable::search(string first)
+{
+	map<string, struct record>::const_iterator it = std::find_if(table_.begin(), table_.end(), keyToValue(first));
+	return it != table_.end() ? it->second : nullRecord;
+}
+
+string HashTable::parseInstruction(string buffer, unsigned int& location)
+{
+	record temp;
+	stringstream ss, tmpISS;
+	unsigned int locationPlus;
+	string::size_type pos;
+	unsigned int num;
+	string result, tmpSearch;
+    
+	tmpISS.str(buffer);
+	for(tmpISS >> tmpSearch; !tmpISS.eof(); tmpISS >> tmpSearch)
+	{
+		temp = this->search(tmpSearch);
+		if(temp.type != NOT) break;
+	}
+    
+	if(temp.type == Instruction)
+	{
+		locationPlus = 0x3;
+		ss << uppercase << hex << setw(4) << std::setfill('0') << location << ", ";
+		result.append(ss.str());
+		ss.str("");
+		ss.clear();
+		ss << temp.mnemonic << ", ";
+		result.append(ss.str());
+		ss.str("");
+		ss.clear();
+		ss << hex << setw(2) << std::setfill('0') << (temp.opcode + 0);
+		result.append(ss.str());
+		location += locationPlus;
+		return result;
+	}
+	else if(temp.type == AssemblerDirectives)
+	{
+		if(temp.mnemonic == "BYTE")
+		{
+			string data;
+			pos = buffer.find(temp.mnemonic);
+			buffer.erase(0, pos+4);
+			pos = 0;
+			while(buffer[pos]==' ') pos++;
+			buffer.erase(0, pos);
+			tmpISS.str(buffer);
+			tmpISS >> data;
+			if(data[0]=='C') num = data.size()-3;
+			else if(data[0]=='X') num = (data.size()-3)/2;
+			else
+			{
+				tmpISS.str(data);
+				tmpISS >> num;
+			}
+			locationPlus = num;
+			ss << uppercase << hex << location << ", ";
+			result.append(ss.str());
+			ss.str("");
+			ss.clear();
+			ss << temp.mnemonic;
+			result.append(ss.str());
+			location += locationPlus;
+			return result;
+		}
+		else if(temp.mnemonic == "WORD")
+		{
+			locationPlus = 0x3;
+			ss << uppercase << hex << location << ", ";
+			result.append(ss.str());
+			ss.str("");
+			ss.clear();
+			ss << temp.mnemonic;
+			result.append(ss.str());
+			location += locationPlus;
+			return result;
+		}
+		else if(temp.mnemonic == "RESB")
+		{
+			pos = buffer.find(temp.mnemonic);
+			buffer.erase(0, pos+4);
+			pos = 0;
+			while(buffer[pos]==' ') pos++;
+			buffer.erase(0, pos);
+			tmpISS.str(buffer);
+			tmpISS >> num;
+			locationPlus = num * 0x1;
+			ss << uppercase << hex << location << ", ";
+			result.append(ss.str());
+			ss.str("");
+			ss.clear();
+			ss << temp.mnemonic;
+			result.append(ss.str());
+			location += locationPlus;
+			return result;
+		}
+		else if(temp.mnemonic == "RESW")
+		{
+			pos = buffer.find(temp.mnemonic);
+			buffer.erase(0, pos+4);
+			pos = 0;
+			while(buffer[pos]==' ') pos++;
+			buffer.erase(0, pos);
+			tmpISS.str(buffer);
+			tmpISS >> num;
+			locationPlus = num * 0x3;
+			ss << uppercase << hex << location << ", ";
+			result.append(ss.str());
+			ss.str("");
+			ss.clear();
+			ss << temp.mnemonic;
+			result.append(ss.str());
+			location += locationPlus;
+			return result;
+		}
+		else
+		{
+			ss << uppercase << hex << location << ", END";
+			result.append(ss.str());
+			return result;
+		}
+	}
+	else
+	{
+		result = " *";
+		return result;
+	}
 }
 
 int main()
 {
 	HashTable table;
-	//table.readOptabFromFile("optab.txt");
-	table.insert(AssemblerDirectives, "START", 0x01, NULL);
-	table.insert(AssemblerDirectives, "END", 0x02, NULL);
-	table.insert(AssemblerDirectives, "BYTE", 0x03, NULL);
-	table.insert(AssemblerDirectives, "WORD", 0x04, NULL);
-	table.insert(AssemblerDirectives, "RESB", 0x05, NULL);
-	table.insert(AssemblerDirectives, "RESW", 0x06, NULL);
-
-	table.search("LDA");
-	table.search("STA");
-	table.search("JSUB");
-	table.search("COMP");
-	table.search("LDX");
-	table.search("START");
-	table.search("END");
-	table.search("RESW");
-	table.search("RESR");
-	
-	table.Delete("LDA");
-	table.search("LDA");
-
+    
+	ifstream sourceFile("example2-1.asm");
+	ofstream outputFile("hw2-3.tmp");
+	int line_count = 1;
+	string buffer;
+	string::size_type pos;
+	stringstream ss;
+	unsigned int location;
+    
+	std::getline(sourceFile, buffer);
+	pos = buffer.find("START");
+	buffer.erase(0, pos+5);
+	pos = 0;
+	while(buffer[pos] == ' ') pos++;
+	buffer.erase(0, pos);
+	ss.str(buffer);
+	ss >> std::hex >> location;
+	outputFile << setw(2) << line_count << ": ";
+	line_count++;
+	outputFile.setf(std::ios_base::hex, std::ios::basefield);
+	outputFile << location << ", START" << endl;
+	outputFile.unsetf(std::ios::hex);
+	while( std::getline(sourceFile, buffer) )
+	{
+		outputFile << setw(2) << line_count << ": ";
+		outputFile << table.parseInstruction(buffer, location) << endl;
+		line_count++;
+	}
+    
+	sourceFile.close();
+	outputFile.close();
 	return 0;
 }
