cda0f96ad12c87fbcb8a6ae567654c5d8fa4320e
ltroot <xi2an.chen@gmail.com>
2014-04-26 17:45:43 +0800

    change architecture: add socket.io,jquery; new feature: return status

--- rpi-healthcare/server.js
+++ rpi-healthcare/server.js
@@ -1,36 +1,60 @@
 var http = require('http'),
+	url = require('url'),
 	fs = require('fs'),
-	qs = require('querystring');
+	qs = require('querystring'),
+	io = require('socket.io');
 
-server = http.createServer(function(request, resource) {
-	fs.readFile(__dirname + '/client.html', function(error, data){
-		resource.write(data);
-		
-		if(request.method == 'POST') {
-			var formData = '';
-			
-			request.on('data', function(data) {
-				formData += data;
-			});
-			
-			request.on('end', function() {
-				var post = qs.parse(formData);
-				for(key in post) {
-					var command = post[key];
-					if(command.match('run')) {
-						console.log(command);
-						// do something
-					} else if (command.match('rotate')) {
-						console.log(command);
-						// do something
-					} else {
-						console.log('Error: command not found');
-					}
+var stat;
+
+var server = http.createServer(function (request, response) {
+	var path = url.parse(request.url).pathname;
+
+	switch(path) {
+		case '/':
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write('Index');
+			response.end();
+			break;
+		case '/client':
+			fs.readFile('./client.html', function (error, data) {
+				response.write(data);
+				//response.end();
+
+				if(request.method == 'POST') {
+					var formData = '';
+
+					request.on('data', function (data) {
+						formData += data;
+					});
+
+					request.on('end', function () {
+						var post = qs.parse(formData);
+						for(key in post) {
+							var command = post[key];
+							if(command.match('run')) {
+								stat = 'run';
+							} else if(command.match('rotate')) {
+								stat = 'rotate';
+							} else {
+								stat = 'Error: command not found';
+							}
+						}
+					});
 				}
-				resource.end();
+				response.end();
 			});
-		}
-	});
+			break;
+		default:
+			response.writeHead(404);
+			response.write("404 - Page not found.");
+			response.end();
+			break;
+	}
 });
 
-server.listen(8080, '127.0.0.1');
+server.listen(8080);
+var serv_io = io.listen(server);
+
+serv_io.sockets.on('connection', function (socket) {
+	socket.emit('updateStatus', {'stat': stat});
+});
